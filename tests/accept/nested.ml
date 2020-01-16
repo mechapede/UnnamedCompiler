f3a8a3357034a9a85f7967bc19f48735d1da4067
Mechapede <26446679+mechapede@users.noreply.github.com>
2020-01-16 15:55:42 -0800

    Added more tests

---
+++ tests/accept/nested.ml
@@ -0,0 +1,21 @@
+void main () {
+    int x;
+    x = 11;
+    if ( 1 == 1 ) {
+        if ( 'a' == 'a' ) {
+            while( x == 1 ) {
+                while ( 2 == 2 ) {
+                
+                }
+            }
+        } 
+        
+    } else {
+
+    }
+    print foo(foo(foo(2)));
+}
+
+int foo ( int x ) {
+    return x + 1;
+}
---
+++ tests/reject/bad_nest.ml
@@ -0,0 +1,5 @@
+void main () {
+    int foo () {
+        return 2;
+    }
+}
